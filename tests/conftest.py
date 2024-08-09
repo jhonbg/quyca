f27060e7eb038c1a2dab0df2b3a19c918989ce07
joselomanuelo <joselomanuelo@gmail.com>
2024-08-09 07:39:06 -0500

    test: conftest implementation and basic worldmap test

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,9 +18,3 @@ def client(app):
     client = app.test_client()
 
     yield client
-
-
-def test_it_can_search_by_institution(client):
-    response = client.get('/quyca/search/affiliations/institution?keywords=universidad+de+antioquia')
-
-    assert response.status_code == 200

e1f8e94e59d9b89dd272e59ca376e3756347c51b
joselomanuelo <joselomanuelo@gmail.com>
2024-08-13 12:41:33 -0500

    test: search by group

--- tests/feature/affiliations/coauthorships_worldmap_test.py
+++ tests/feature/affiliations/coauthorships_worldmap_test.py
@@ -8,7 +8,15 @@ def test_it_can_send_coauthorships_worldmap_by_institution(client):
 
 def test_it_can_send_coauthorships_worldmap_by_department(client):
     response = client.get(
-        '/app/affiliation/department/66b583a37102ee7e0fcda0ca/research/products?plot''=collaboration_worldmap'
+        '/app/affiliation/department/66b583a37102ee7e0fcda0f4/research/products?plot''=collaboration_worldmap'
+    )
+
+    assert response.status_code == 200
+
+
+def test_it_can_send_coauthorships_worldmap_by_group(client):
+    response = client.get(
+        '/app/affiliation/group/66b583cb7102ee7e0fcda1b0/research/products?plot''=collaboration_worldmap'
     )
 
     assert response.status_code == 200

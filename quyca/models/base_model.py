79c7a2a0bb97a06290efd6a4957edd4e7226b9c5
joselomanuelo <joselomanuelo@gmail.com>
2024-08-14 17:47:13 -0500

    feat: use odmantic ObjectId model for pydantic use

---
+++ quyca/models/base_model.py
@@ -0,0 +1,58 @@
+from typing import Any, Callable, Union
+from pydantic import GetJsonSchemaHandler
+from bson import ObjectId, errors
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import core_schema
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Callable[[Any], core_schema.CoreSchema],
+    ) -> core_schema.CoreSchema:
+        def validate_from_string_or_bytes(value: Union[str, bytes]) -> ObjectId:
+            try:
+                return ObjectId(value)
+            except errors.InvalidId:
+                raise ValueError("Invalid ObjectId")
+
+        from_string_or_bytes_schema = core_schema.chain_schema(
+            [
+                core_schema.union_schema(
+                    [
+                        core_schema.str_schema(),
+                        core_schema.bytes_schema(),
+                    ]
+                ),
+                core_schema.no_info_plain_validator_function(
+                    validate_from_string_or_bytes
+                ),
+            ]
+        )
+
+        return core_schema.json_or_python_schema(
+            json_schema=from_string_or_bytes_schema,
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(ObjectId),
+                    from_string_or_bytes_schema,
+                ],
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                str, when_used="json"
+            ),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _schema: core_schema.CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        json_schema = handler(core_schema.str_schema())
+        json_schema.update(
+            examples=["5f85f36d6dfecacc68428a46", "ffffffffffffffffffffffff"],
+            example="5f85f36d6dfecacc68428a46",
+        )
+        return json_schema
+

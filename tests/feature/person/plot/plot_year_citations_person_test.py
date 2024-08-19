0b87ab4a3a27074daa50af5ce294a4984bce378b
joselomanuelo <joselomanuelo@gmail.com>
2024-08-19 15:32:22 -0500

    test: person plots

---
+++ tests/feature/person/plot/plot_year_citations_person_test.py
@@ -0,0 +1,11 @@
+from repositories.mongo import database
+
+random_person_id = database["person"].aggregate([{ '$sample': { 'size': 1 } }]).next()["_id"]
+
+
+def test_it_can_plot_year_citations_by_institution(client):
+    response = client.get(
+        f"/app/person/{random_person_id}/research/products?plot=year_citations"
+    )
+
+    assert response.status_code == 200

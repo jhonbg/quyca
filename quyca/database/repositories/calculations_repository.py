1da9e2b83cb617f09720b461eea4c22dbd075951
joselomanuelo <joselomanuelo@gmail.com>
2024-08-24 10:24:15 -0500

    feat: implement calculations exceptions

--- quyca/database/repositories/calculations_repository.py
+++ quyca/database/repositories/calculations_repository.py
@@ -3,6 +3,8 @@ from bson import ObjectId
 from database.models.affiliation_calculations_model import AffiliationCalculations
 from database.models.person_calculations_model import PersonCalculations
 from database.mongo import calculations_database
+from exceptions.affiliation_calculations_exception import AffiliationCalculationsException
+from exceptions.person_calculations_exception import PersonCalculationsException
 
 
 class CalculationsRepository:
@@ -11,10 +13,16 @@ class CalculationsRepository:
     def get_person_calculations(person_id: str) -> PersonCalculations:
         person_calculations = calculations_database["person"].find_one({"_id": ObjectId(person_id)})
 
+        if not person_calculations:
+            raise PersonCalculationsException(person_id)
+
         return PersonCalculations(**person_calculations)
 
     @staticmethod
     def get_affiliation_calculations(affiliation_id: str) -> AffiliationCalculations:
         affiliation_calculations = calculations_database["affiliations"].find_one({"_id": ObjectId(affiliation_id)})
+
+        if not affiliation_calculations:
+            raise AffiliationCalculationsException(affiliation_id)
 
         return AffiliationCalculations(**affiliation_calculations)

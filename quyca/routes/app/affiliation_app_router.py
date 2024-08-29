1927e13a49dac7d1008bfe7190e5200f796e7ddc
joselomanuelo <joselomanuelo@gmail.com>
2024-08-29 17:56:42 -0500

    feat: delete unused code

--- quyca/routes/app/affiliation_app_router.py
+++ quyca/routes/app/affiliation_app_router.py
@@ -1,6 +1,4 @@
 import json
-import io
-import csv
 
 from flask import Blueprint, request, Response, jsonify
 from pydantic import ValidationError
@@ -94,66 +92,3 @@ def get_csv_by_affiliation(affiliation_type: str, affiliation_id: str):
         return response
     except Exception as e:
         return Response(json.dumps({"error": str(e)}), 400, mimetype="application/json")
-
-
-@affiliation_app_router.route("/<typ>/<id>/<section>/csv", methods=["GET"])
-@affiliation_app_router.route("/<typ>/<id>/<section>/<tab>/csv", methods=["GET"])
-def get_affiliation_csv(
-    id: str | None,
-    typ: str | None = None,
-    section: str | None = "info",
-    tab: str | None = None,
-):
-    result = affiliation_service.get_research_products_csv(id=id, typ=typ)
-    if result:
-        config = {
-            "title": {
-                "name": "titulo",
-            },
-            "authors": {
-                "name": "autores",
-                "fields": ["full_name"],
-                "config": {"full_name": {"name": "full_name"}},
-            },
-            "lenguage": {"name": "lengua"},
-            "citations_count": {
-                "name": "veces citado",
-                "fields": ["count"],
-                "config": {"count": {"name": "count"}},
-            },
-            "date_published": {
-                "name": "fecha publicación",
-                "expresion": "datetime.date.fromtimestamp(value).strftime('%Y-%m-%d')",
-            },
-            "volume": {"name": "volumen"},
-            "issue": {"name": "issue"},
-            "start_page": {"name": "página inicial"},
-            "end_page": {"name": "página final"},
-            "year_published": {"name": "año de publicación"},
-            "types": {"name": "tipo de producto", "fields": ["type"]},
-            "subject_names": {"name": "temas"},
-            "doi": {"name": "doi"},
-            "source_name": {"name": "revista"},
-            "scimago_quartile": {"name": "cuartil de scimago"},
-        }
-        flat_data_list = flatten_json_list(result, config, 1)
-        all_keys = []
-        for item in flat_data_list:
-            all_keys += [key for key in item.keys() if key not in all_keys]
-
-        output = io.StringIO()
-        csv_writer = csv.DictWriter(output, fieldnames=all_keys)
-        csv_writer.writeheader()
-        csv_writer.writerows(flat_data_list)
-
-        response = Response(output.getvalue(), content_type="text/csv")
-        response.headers["Content-Disposition"] = "attachment; filename=affiliation.csv"
-
-    else:
-        response = Response(
-            response=json.dumps({}, cls=JsonEncoder),
-            status=204,
-            mimetype="application/json",
-        )
-
-    return response
--- quyca/routes/app/person_app_router.py
+++ quyca/routes/app/person_app_router.py
@@ -1,6 +1,4 @@
 import json
-import io
-import csv
 
 from bson.errors import InvalidId
 from flask import Blueprint, request, Response, Request
@@ -11,7 +9,6 @@ from services import new_person_service
 from services.work import work_service
 from schemas.work import WorkQueryParams
 from utils.encoder import JsonEncoder
-from utils.flatten_json import flatten_json_list
 
 person_app_router = Blueprint("person_app_router", __name__)
 
@@ -99,64 +96,3 @@ def get_csv_by_person(person_id: str):
         return response
     except Exception as e:
         return Response(json.dumps({"error": str(e)}), 400, mimetype="application/json")
-
-
-@person_app_router.route("/<id>/<section>/<tab>/csv", methods=["GET"])
-def get_person_csv(
-    id: str | None = None, section: str | None = "info", tab: str | None = None
-):
-    result = work_service.get_research_products_by_author_csv(author_id=id)
-    if result:
-        config = {
-            "title": {
-                "name": "titulo",
-            },
-            "authors": {
-                "name": "autores",
-                "fields": ["full_name"],
-                "config": {"full_name": {"name": "full_name"}},
-            },
-            "lenguage": {"name": "lengua"},
-            "citations_count": {
-                "name": "veces citado",
-                "fields": ["count"],
-                "config": {"count": {"name": "count"}},
-            },
-            "date_published": {
-                "name": "fecha publicación",
-                "expresion": "datetime.date.fromtimestamp(value).strftime('%Y-%m-%d')",
-            },
-            "volume": {"name": "volumen"},
-            "issue": {"name": "issue"},
-            "start_page": {"name": "página inicial"},
-            "end_page": {"name": "página final"},
-            "year_published": {"name": "año de publicación"},
-            "types": {"name": "tipo de producto", "fields": ["type"]},
-            "subjects": {
-                "name": "temas",
-                "fields": ["name"],
-                "config": {"name": {"name": "name"}},
-            },
-            "doi": {"name": "doi"},
-            "source_name": {"name": "revista"},
-        }
-        flat_data_list = flatten_json_list(result, config, 1)
-        all_keys = []
-        for item in flat_data_list:
-            all_keys += [key for key in item.keys() if key not in all_keys]
-
-        output = io.StringIO()
-        csv_writer = csv.DictWriter(output, fieldnames=all_keys)
-        csv_writer.writeheader()
-        csv_writer.writerows(flat_data_list)
-
-        response = Response(output.getvalue(), content_type="text/csv")
-        response.headers["Content-Disposition"] = "attachment; filename=person.csv"
-
-    else:
-        response = Response(
-            response=json.dumps({}, cls=JsonEncoder),
-            status=204,
-            mimetype="application/json",
-        )
-    return response

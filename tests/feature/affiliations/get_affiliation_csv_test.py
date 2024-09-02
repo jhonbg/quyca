1f40f57c47be8189dc3d6cdafaa011dea32ae005
joselomanuelo <joselomanuelo@gmail.com>
2024-09-02 14:15:47 -0500

    test: works csv by affiliation

--- tests/feature/affiliations/get_affiliation_csv_test.py
+++ tests/feature/affiliations/get_affiliation_csv_test.py
@@ -1,27 +1,49 @@
 from database.mongo import database
 
-random_affiliation_id = "66b583d97102ee7e0fcda396"
+random_institution_id = (
+    database["affiliations"]
+    .aggregate([{"$match": {"types.type": "education"}}, {"$sample": {"size": 1}}])
+    .next()["_id"]
+)
+
+random_department_id = (
+    database["affiliations"]
+    .aggregate([{"$match": {"types.type": "department"}}, {"$sample": {"size": 1}}])
+    .next()["_id"]
+)
+
+random_faculty_id = (
+    database["affiliations"]
+    .aggregate([{"$match": {"types.type": "faculty"}}, {"$sample": {"size": 1}}])
+    .next()["_id"]
+)
+
+random_group_id = (
+    database["affiliations"]
+    .aggregate([{"$match": {"types.type": "group"}}, {"$sample": {"size": 1}}])
+    .next()["_id"]
+)
 
 
-def test_get_institution_by_id(client):
-    response = client.get(f"/app/affiliation/institution/{random_affiliation_id}/csv")
+def test_get_csv_works_by_institution(client):
+    response = client.get(f"/app/affiliation/institution/{random_institution_id}/csv")
 
     assert response.status_code == 200
 
 
-def test_get_faculty_by_id(client):
-    response = client.get(f"/app/affiliation/faculty/{random_affiliation_id}/csv")
+def test_get_csv_works_by_faculty(client):
+    response = client.get(f"/app/affiliation/faculty/{random_faculty_id}/csv")
 
     assert response.status_code == 200
 
 
-def test_get_department_by_id(client):
-    response = client.get(f"/app/affiliation/department/{random_affiliation_id}/csv")
+def test_get_csv_works_by_department(client):
+    response = client.get(f"/app/affiliation/department/{random_department_id}/csv")
 
     assert response.status_code == 200
 
 
-def test_get_group_by_id(client):
-    response = client.get(f"/app/affiliation/group/{random_affiliation_id}/csv")
+def test_get_csv_works_by_group(client):
+    response = client.get(f"/app/affiliation/group/{random_group_id}/csv")
 
     assert response.status_code == 200

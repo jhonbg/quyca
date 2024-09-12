5a3626c97d56f256cbfa285a94cdb55879801884
joselomanuelo <joselomanuelo@gmail.com>
2024-09-12 11:22:53 -0500

    feat: set person citations count backend etl

--- quyca/routes/etl_router.py
+++ quyca/routes/etl_router.py
@@ -14,3 +14,12 @@ def set_person_products_count():
         return Response(json.dumps("The etl is done"), 200, mimetype="application/json")
     except Exception as e:
         return Response(json.dumps({"error": str(e)}), 400, mimetype="application/json")
+
+
+@etl_router.route("/set_person_citations_count", methods=["GET"])
+def set_person_citations_count():
+    try:
+        etl_service.set_person_citations_count()
+        return Response(json.dumps("The etl is done"), 200, mimetype="application/json")
+    except Exception as e:
+        return Response(json.dumps({"error": str(e)}), 400, mimetype="application/json")
--- quyca/services/etl_service.py
+++ quyca/services/etl_service.py
@@ -3,3 +3,7 @@ from database.repositories import etl_repository
 
 def set_person_products_count():
     etl_repository.set_person_products_count()
+
+
+def set_person_citations_count():
+    etl_repository.set_person_citations_count()

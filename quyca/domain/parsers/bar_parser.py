950a8e752ee1bc867b277efc075ff67c7a90b3be
joselomanuelo <joselomanuelo@gmail.com>
2024-09-29 11:46:48 -0500

    feat: order by year value some bar plots

--- quyca/domain/parsers/bar_parser.py
+++ quyca/domain/parsers/bar_parser.py
@@ -18,7 +18,7 @@ def parse_annual_evolution_by_scienti_classification(works: Generator) -> dict:
         for year, work_types in data.items()
         for work_type, count in work_types.items()
     ]
-    return {"plot": sorted(plot, key=lambda x: x.get("x"))}
+    return {"plot": sorted(plot, key=lambda x: (x.get("x"), -x.get("y")))}
 
 
 def parse_affiliations_by_product_type(data: CommandCursor) -> dict:
@@ -47,14 +47,8 @@ def parse_annual_articles_open_access(works: Generator) -> dict:
     data: defaultdict = defaultdict(lambda: {"Abierto": 0, "Cerrado": 0, "Sin información": 0})
     for work in works:
         access_type = "Abierto" if work.open_access.is_open_access else "Cerrado"
-        if work.open_access.is_open_access is None and not work.year_published:
-            data["Sin año"]["Sin información"] += 1
-            continue
         if work.open_access.is_open_access is None:
             data[work.year_published]["Sin información"] += 1
-            continue
-        if work.year_published is None:
-            data["Sin año"][access_type] += 1
             continue
         data[work.year_published][access_type] += 1
     plot = [
@@ -62,20 +56,14 @@ def parse_annual_articles_open_access(works: Generator) -> dict:
         for year, counts in data.items()
         for access_type, count in counts.items()
     ]
-    return {"plot": sorted(plot, key=lambda x: float("inf") if x.get("x") == "Sin año" else x.get("x"))}
+    return {"plot": sorted(plot, key=lambda x: x.get("x"))}
 
 
 def parse_annual_articles_by_top_publishers(works: Generator) -> dict:
     data: defaultdict = defaultdict(lambda: defaultdict(int))
     for work in works:
-        if not work.source.publisher and not work.year_published:
-            data["Sin año"]["Sin información"] += 1
-            continue
         if not work.source.publisher:
             data[work.year_published]["Sin información"] += 1
-            continue
-        if not work.year_published:
-            data["Sin año"][work.source.publisher.name] += 1
             continue
         data[work.year_published][work.source.publisher.name] += 1
     plot = [
@@ -83,7 +71,7 @@ def parse_annual_articles_by_top_publishers(works: Generator) -> dict:
         for year, publishers in data.items()
         for publisher, count in publishers.items()
     ]
-    return {"plot": sorted(plot, key=lambda x: float("inf") if x.get("x") == "Sin año" else x.get("x"))}
+    return {"plot": sorted(plot, key=lambda x: (x.get("x"), -x.get("y")))}
 
 
 def parse_annual_apc_expenses(works: Generator) -> dict:

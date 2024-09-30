38a7eeb4f8976d37661684b79e6e248f0fccaf88
joselomanuelo <joselomanuelo@gmail.com>
2024-09-30 17:11:08 -0500

    fix: include id

--- quyca/infrastructure/repositories/plot_repository.py
+++ quyca/infrastructure/repositories/plot_repository.py
@@ -133,7 +133,7 @@ def get_groups_citations_count_by_faculty_or_department(affiliation_id: str) -> 
                             "types.type": "group",
                         },
                     },
-                    {"$project": {"_id": 0, "name": {"$first": "$names.name"}, "citations_count": 1}},
+                    {"$project": {"_id": 1, "name": {"$first": "$names.name"}, "citations_count": 1}},
                 ],
             }
         },

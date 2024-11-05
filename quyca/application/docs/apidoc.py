395362c92543ddd986bd18fa2ab2a1a56c8553ef
joselomanuelo <joselomanuelo@gmail.com>
2024-11-05 10:25:37 -0500

    refactor: correct routes on apidoc generation

--- quyca/application/docs/apidoc.py
+++ quyca/application/docs/apidoc.py
@@ -3,5 +3,5 @@ from os import getenv
 
 def generate_apidoc() -> None:
     command = (
-        "apidoc -i /quyca/routes " f"-c /quyca/core/docs/apidoc.{getenv('ENVIRONMENT', 'dev')}.json " "-o /quyca/static"
+        "apidoc -i ./quyca/application/routes -c ./quyca/application/docs/apidoc.dev.json -o ./quyca/application/static"
     )

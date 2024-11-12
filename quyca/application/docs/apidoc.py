fe33da16dccc3f7174f18b6573b14aa36c2ab421
joselomanuelo <joselomanuelo@gmail.com>
2024-11-12 13:51:10 -0500

    refactor: apidoc info

--- quyca/application/docs/apidoc.py
+++ quyca/application/docs/apidoc.py
@@ -1,7 +1,2 @@
-from os import getenv
-
-
 def generate_apidoc() -> None:
-    command = (
-        "apidoc -i ./quyca/application/routes -c ./quyca/application/docs/apidoc.dev.json -o ./quyca/application/static"
-    )
+    command = "apidoc -i ./quyca/application/routes -c ./quyca/application/docs/apidoc.prod.json -o ./quyca/application/static"

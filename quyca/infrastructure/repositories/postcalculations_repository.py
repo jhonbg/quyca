b14faa1e006f86af6bfd3d2847b3aa918daa409f
joselomanuelo <joselomanuelo@gmail.com>
2024-11-19 11:06:56 -0500

    feat: pipeline for works authors affiliations country

---
+++ quyca/infrastructure/repositories/postcalculations_repository.py
@@ -0,0 +1,88 @@
+from infrastructure.mongo import database
+
+
+def set_works_authors_affiliations_country():  # type: ignore
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "affiliations",
+                "localField": "authors.affiliations.id",
+                "foreignField": "_id",
+                "as": "affiliations_data",
+                "pipeline": [{"$project": {"_id": 1, "addresses.country": 1}}],
+            }
+        },
+        {
+            "$addFields": {
+                "authors": {
+                    "$map": {
+                        "input": "$authors",
+                        "as": "author",
+                        "in": {
+                            "$mergeObjects": [
+                                "$$author",
+                                {
+                                    "affiliations": {
+                                        "$map": {
+                                            "input": "$$author.affiliations",
+                                            "as": "affiliation",
+                                            "in": {
+                                                "$mergeObjects": [
+                                                    "$$affiliation",
+                                                    {
+                                                        "country": {
+                                                            "$let": {
+                                                                "vars": {
+                                                                    "matchedAffiliation": {
+                                                                        "$arrayElemAt": [
+                                                                            {
+                                                                                "$filter": {
+                                                                                    "input": "$affiliations_data",
+                                                                                    "as": "affiliation_data",
+                                                                                    "cond": {
+                                                                                        "$eq": [
+                                                                                            "$$affiliation.id",
+                                                                                            "$$affiliation_data._id",
+                                                                                        ]
+                                                                                    },
+                                                                                }
+                                                                            },
+                                                                            0,
+                                                                        ]
+                                                                    }
+                                                                },
+                                                                "in": {
+                                                                    "$ifNull": [
+                                                                        {
+                                                                            "$arrayElemAt": [
+                                                                                "$$matchedAffiliation.addresses.country",
+                                                                                0,
+                                                                            ]
+                                                                        },
+                                                                        None,
+                                                                    ]
+                                                                },
+                                                            }
+                                                        }
+                                                    },
+                                                ]
+                                            },
+                                        }
+                                    }
+                                },
+                            ]
+                        },
+                    }
+                }
+            }
+        },
+        {"$project": {"affiliations_data": 0}},
+        {
+            "$merge": {
+                "into": "works",
+                "whenMatched": "merge",
+                "whenNotMatched": "fail",
+            }
+        },
+    ]
+    database["works"].aggregate(pipeline)

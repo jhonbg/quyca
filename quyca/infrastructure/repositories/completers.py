6391359c0229445cc7f8069eb8521efc71299a7d
Omar Zapata <andresete.chaos@gmail.com>
2025-03-28 12:20:07 -0500

    [elasticsearch] added full_name for autocompleter instead text, name have to be taken from it

--- quyca/infrastructure/repositories/completers.py
+++ quyca/infrastructure/repositories/completers.py
@@ -14,4 +14,10 @@ def person_completer(text: str) -> List[Dict[str, Any]]:
     }
 
     response = es_database.search(index=settings.ES_PERSON_COMPLETER_INDEX, body=query)
+    for opt in response["suggest"]["name_suggest"][0]["options"]:
+        full_name = ""
+        for name in opt["_source"]["full_name"]["input"]:
+            if len(full_name) < len(name):
+                full_name = name
+        opt["full_name"] = full_name
     return response["suggest"]["name_suggest"][0]["options"]

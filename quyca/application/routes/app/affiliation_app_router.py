c59ae4226f81fe6138c158607881a1b475ca50ff
fedevergara <fedevego15@gmail.com>
2025-08-13 16:17:06 -0500

    News for authors by affiliation has been implemented.

--- quyca/application/routes/app/affiliation_app_router.py
+++ quyca/application/routes/app/affiliation_app_router.py
@@ -13,6 +13,7 @@ from domain.services import (
     affiliation_plot_service,
     csv_service,
     patent_service,
+    news_service,
 )
 
 affiliation_app_router = Blueprint("affiliation_app_router", __name__)
@@ -201,6 +202,45 @@ def get_affiliation_research_projects(affiliation_id: str, affiliation_type: str
     try:
         query_params = QueryParams(**request.args)
         data = project_service.get_projects_by_affiliation(affiliation_id, query_params)
+        return jsonify(data)
+    except Exception as e:
+        capture_exception(e)
+        return jsonify({"error": str(e)}), 400
+
+
+"""
+@api {get} /app/affiliations/:affiliation_type/:affiliation_id/research/news Get news by affiliation
+@apiName GetAffiliationNews
+@apiGroup Affiliation
+@apiVersion 1.0.0
+@apiDescription Obtiene las noticias asociadas a los autores de una afiliación.
+@apiParam {String} affiliation_type Tipo de afiliación (ej. "institution", "department").
+@apiParam {String} affiliation_id ID de la afiliación.
+"""
+
+
+@affiliation_app_router.route("/<affiliation_type>/<affiliation_id>/research/news")
+def get_affiliation_research_news(affiliation_id: str, affiliation_type: str) -> Response:
+    """
+    Flask route to retrieve news for a given affiliation ID.
+
+    Parses query parameters, retrieves the corresponding news from the service layer,
+    and returns the data as a JSON response.
+
+    Route:
+    ------
+    GET /affiliations/<affiliation_type>/<affiliation_id>/research/news
+
+    Parameters:
+    -----------
+    affiliation_id : str
+        The ID of the affiliation for which news is being retrieved.
+    affiliation_type : str
+        The type of the affiliation (e.g., "institution", "department").
+    """
+    try:
+        query_params = QueryParams(**request.args)
+        data = news_service.get_news_by_affiliation(affiliation_id, affiliation_type, query_params)
         return jsonify(data)
     except Exception as e:
         capture_exception(e)

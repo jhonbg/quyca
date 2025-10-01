fdcc206cc4e5f79bbe3ef152a66945793ee777f8
Esteban <estebancg12@hotmail.com>
2025-10-01 13:59:18 -0500

    [fix] Update API endpoint paths in affiliation tests to include the correct prefix

--- tests/feature/affiliations/get_works_by_affiliation_api_expert_test.py
+++ tests/feature/affiliations/get_works_by_affiliation_api_expert_test.py
@@ -8,7 +8,7 @@ def test_get_works_by_institution_api_expert(client):
         .next()["_id"]
     )
     response = client.get(
-        f"/affiliation/institution/{random_institution_id}/research/products?max=10&page=1&sort=citations_desc"
+        f"/api/affiliation/institution/{random_institution_id}/research/products?max=10&page=1&sort=citations_desc"
     )
     assert response.status_code == 200
 
@@ -20,7 +20,7 @@ def test_get_works_by_faculty_api_expert(client):
         .next()["_id"]
     )
     response = client.get(
-        f"/affiliation/faculty/{random_faculty_id}/research/products?max=10&page=1&sort=citations_desc"
+        f"/api/affiliation/faculty/{random_faculty_id}/research/products?max=10&page=1&sort=citations_desc"
     )
     assert response.status_code == 200
 
@@ -32,7 +32,7 @@ def test_get_works_by_department_api_expert(client):
         .next()["_id"]
     )
     response = client.get(
-        f"/affiliation/department/{random_department_id}/research/products?max=10&page=1&sort=citations_desc"
+        f"/api/affiliation/department/{random_department_id}/research/products?max=10&page=1&sort=citations_desc"
     )
     assert response.status_code == 200
 
@@ -43,5 +43,5 @@ def test_get_works_by_group_api_expert(client):
         .aggregate([{"$match": {"types.type": "group"}}, {"$sample": {"size": 1}}])
         .next()["_id"]
     )
-    response = client.get(f"/affiliation/group/{random_group_id}/research/products?max=10&page=1&sort=citations_desc")
+    response = client.get(f"/api/affiliation/group/{random_group_id}/research/products?max=10&page=1&sort=citations_desc")
     assert response.status_code == 200
